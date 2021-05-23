75844902dc3c6af4252c42bcfb536439127044f2
svn.tmr <svn.tmr@bk.ru>
2021-05-23 15:49:46 +0300

    added solution for exercise 1.5

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,14 @@
 # bounce.py
 #
 # Exercise 1.5
+HEIGHT_LOOSING_RATIO = 3 / 5
+JUMPS_TO_SHOW = 10
+
+jump = 1
+height = 100  # Meters
+
+while jump <= JUMPS_TO_SHOW:
+    height = height * HEIGHT_LOOSING_RATIO
+    print(jump, round(height, 4))
+    jump = jump + 1
+
