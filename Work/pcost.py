9a5524bdfcab9c62cb3274af3377854e3b92cfba
svn.tmr <svn.tmr@bk.ru>
2021-05-30 20:07:22 +0300

    added solution for exercise 1.33

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,11 +1,30 @@
 # pcost.py
 #
 # Exercise 1.27
-total_cost = 0.0  # Total portfolio cost in dollars
+import csv
+import sys
 
-with open('Data/portfolio.csv', 'rt') as portfolio:
-    next(portfolio)  # Skip headings
-    for line in portfolio:
-        share_amount, share_price = line.split(',')[1:]
-        total_cost += float(share_amount) * float(share_price)
-print(f'Total cost {total_cost}')
+
+def portfolio_cost(filename):
+    """Calculates the total cost of portfolio"""
+    total_cost = 0.0  # Total portfolio cost in dollars
+
+    with open(filename, 'rt') as file:
+        portfolio = csv.reader(file)
+        next(portfolio)  # Skip headings
+        for line in portfolio:
+            try:
+                share_amount, share_price = line[1:]
+                total_cost += float(share_amount) * float(share_price)
+            except ValueError:
+                print('Bad file format, this line will be skipped')
+    return total_cost
+
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+cost = portfolio_cost(filename)
+print('Total cost:', cost)
+
