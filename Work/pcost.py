8df7a0c5c2414bbfd1540159c27dda93a685aed7
svn.tmr <svn.tmr@bk.ru>
2021-05-30 15:58:42 +0300

    added solution for exercise 1.27

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,11 @@
 # pcost.py
 #
 # Exercise 1.27
+total_cost = 0.0  # Total portfolio cost in dollars
+
+with open('Data/portfolio.csv', 'rt') as portfolio:
+    next(portfolio)  # Skip headings
+    for line in portfolio:
+        share_amount, share_price = line.split(',')[1:]
+        total_cost += float(share_amount) * float(share_price)
+print(f'Total cost {total_cost}')
