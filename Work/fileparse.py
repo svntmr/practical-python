fcf5937744f78edbc5268f9db9f5ff7b3a3410d3
svn.tmr <svn.tmr@bk.ru>
2021-06-20 19:06:55 +0300

    added solution for exercise 3.10

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -6,9 +6,12 @@ from typing import Optional
 
 
 def parse_csv(filename: str, select: Optional[list] = None, types: Optional[list] = None, has_headers: bool = True,
-              delimiter: str = ';') -> list:
+              delimiter: str = ',', silence_errors=False) -> list:
     """Parse a CSV file into a list of records"""
     with open(filename) as f:
+        if select and not has_headers:
+            raise RuntimeError("select argument requires column headers")
+
         rows = csv.reader(f, delimiter=delimiter)
 
         records = []  # Container for file records
@@ -25,19 +28,23 @@ def parse_csv(filename: str, select: Optional[list] = None, types: Optional[list
             else:
                 indices = []
 
-        for row in rows:
-            if not row:  # Skip rows with no data
-                continue
-            # Type change if specified
-            if types:
-                row = [func(val) for func, val in zip(types, row)]
-            if has_headers:
-                # Filter the row if specific columns were selected
-                if indices:
-                    row = [row[index] for index in indices]
-                record = dict(zip(headers, row))
-            else:
-                record = tuple(row)
-            records.append(record)
+        for line, row in enumerate(rows):
+            try:
+                if not row:  # Skip rows with no data
+                    continue
+                # Type change if specified
+                if types:
+                    row = [func(val) for func, val in zip(types, row)]
+                if has_headers:
+                    # Filter the row if specific columns were selected
+                    if indices:
+                        row = [row[index] for index in indices]
+                    record = dict(zip(headers, row))
+                else:
+                    record = tuple(row)
+                records.append(record)
+            except ValueError as e:
+                if not silence_errors:
+                    print(f'Row {line}:', e)
 
     return records
