d137aae27d112ceaa10d2d4055f3e503b5993459
svn.tmr <svn.tmr@bk.ru>
2021-07-10 19:05:00 +0300

    added solution for exercise 9.5

---
+++ Work/porty-app/porty/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="porty",
+    version="0.0.1",
+    author="Your Name",
+    author_email="you@example.com",
+    description="Practical Python Code",
+    url="none",
+    packages=find_packages(),
+)
